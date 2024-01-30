64befbe0593fd0f445ffa62160e371e99ef03039
Koteshwara Sastry <kotesh-full-access>
2024-01-30 11:08:33 +0530

    Initial commit

---
+++ src/main/java/uk/tw/energy/exceptionhandling/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package uk.tw.energy.exceptionhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        // Log the exception
+        ex.printStackTrace();
+
+        // Return a response with a 404 status code and error message
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Resource not found: " + ex.getRequestURL());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        // Log the exception
+        ex.printStackTrace();
+
+        // Return a response with a 400 status code and error message
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Bad request: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        // Log the exception
+        ex.printStackTrace();
+
+        // Return a response with an internal server error status code and error message
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An error occurred. Please try again later.");
+    }
+}
---
+++ src/test/java/uk/tw/energy/joyofenergy/JoyofenergyApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.tw.energy.joyofenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyofenergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
